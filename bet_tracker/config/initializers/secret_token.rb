84c1f511bded8cc6fc55469a8d571aad85057f9f
Alain Sirois <alainsirois@gmail.com>
2013-11-27 22:38:45 -0500

    first commit

---
+++ bet_tracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetTracker::Application.config.secret_key_base = '5fec71692e586664b54f29e82d1f7cf97aa14b11cdb94524e7e68018142d77fcf35fc6e6da611bd3376f87f2f6b6749c799c5adffdfbfb972744182d0f833f8f'
---
+++ bet_tracker/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetTracker::Application.config.session_store :cookie_store, key: '_bet_tracker_session'
